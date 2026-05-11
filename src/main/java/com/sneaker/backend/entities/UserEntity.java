f8cfbb9d5c39995b71786b3607d0b8a77a0531ba
Pranjalrajput-coder <pranjalrajput46@gmail.com>
2026-05-11 21:11:31 +0530

    FRONTEND CONNECT

--- src/main/java/com/sneaker/backend/entities/UserEntity.java
+++ src/main/java/com/sneaker/backend/entities/UserEntity.java
@@ -1,8 +1,6 @@
 package com.sneaker.backend.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +15,7 @@ import lombok.Setter;
 public class UserEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String username;
